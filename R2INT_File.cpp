473865e2df6e840bc169e687cabc2e2bad531ae8
Numbers633 <78310885+Numbers633@users.noreply.github.com>
2025-08-17 14:52:11 -0500

    Complete saving to R2INT files

--- R2INT_File.cpp
+++ R2INT_File.cpp
@@ -6,26 +6,42 @@
 
 void SaveTor2intFile(R2INTRules& saveRule)
 {
-	std::cout << "Enter the filename to save your rule to: ";
-	std::string saveName = "";
-	std::cin >> saveName;
+    std::cout << "Enter the filename to save your rule to: ";
+    std::string saveName = "";
+    std::cin >> saveName;
 
-	saveName += ".r2int";
-	
-	std::cout << "Saving to " << saveName << std::endl;
+    // Append extension if not already present
+    if (saveName.size() < 6 || saveName.substr(saveName.size() - 6) != ".r2int")
+    {
+        saveName += ".r2int";
+    }
 
-	for (int i = 0; i < 33554432; i++)
-	{
-		if (FindLowestNeighborhoodValue(i) != i)
-		{
-			continue;
-		}
-		if (saveRule.R2MAP[i] == true)
-		{
-			Neighborhood n = ConvertIntToNeighborhood(i);
-			std::cout << n[12] << n[7] << n[8] << n[13] << n[18] << n[17] << n[16] << n[11] << n[6];
-			std::cout << n[2] << n[3] << n[4] << n[9] << n[14] << n[19] << n[24] << n[23] << n[22];
-			std::cout << n[21] << n[20] << n[15] << n[10] << n[5] << n[0] << std::endl;
-		}
-	}
+    std::ofstream outFile(saveName);
+    if (!outFile)
+    {
+        std::cerr << "Error: Could not open " << saveName << " for writing.\n";
+        return;
+    }
+
+    std::cout << "Saving to " << saveName << std::endl;
+
+    for (int i = 0; i < 33554432; i++)
+    {
+        if (FindLowestNeighborhoodValue(i) != i)
+        {
+            continue;
+        }
+        if (saveRule.R2MAP[i] == true)
+        {
+            Neighborhood n = ConvertIntToNeighborhood(i);
+
+            // Instead of cout, write to the file
+            outFile << n[12] << n[7] << n[8] << n[13] << n[18] << n[17] << n[16] << n[11] << n[6];
+            outFile << n[2] << n[3] << n[4] << n[9] << n[14] << n[19] << n[24] << n[23] << n[22];
+            outFile << n[21] << n[20] << n[15] << n[10] << n[5] << n[0] << "\n";
+        }
+    }
+
+    outFile.close();
+    std::cout << "Save complete!" << std::endl;
 }
